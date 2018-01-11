d5ed3bcfe356b9b4fa4e6f016b3391fc3ef9d714
seabbs <s.e.abbott12@gmail.com>
2018-01-11 10:52:38 +0000

    final tweaks to practicals

--- vignettes/practical_1.R
+++
@@ -1,8 +0,0 @@
-## ----packages, include = FALSE-------------------------------------------
-library(tidyverse)
-library(tbinenglanddataclean)
-library(prettypublisher)
-
-## ----plot-incidence, echo = FALSE, message = FALSE, warning = FALSE, dpi= 300, fig.height = 10, fig.width = 10, fig.cap = pretty_figref("incidence_tb", caption = "TB notifications in England and Wales from 2013 to 2016, stratified initially by respiratory/non-respiratory status and from 1982 by pulmonary/non-pulmonary TB. Interventions are highlighted with vertical lines, with linetype denoting the type of intervention.")----
-tbinenglanddataclean::plot_historic_tb_ew()
-
